96b85fbb5261e0853b2a91742cc07f8921245b03
Dennis Rutjes <dennis@jpoint.nl>
2020-02-10 17:20:27 +0100

    Added build binaries

--- build.sh
+++ build.sh
@@ -1,2 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+CGO_ENABLED=0
+GOOS=linux
+GOARCH=darwin
+
+mkdir -p ./build/$GOOS/$GOARCH
+GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o ./build/$GOOS/$GOARCH
+
+CGO_ENABLED=0
+GOOS=linux
+GOARCH=amd64
+
+mkdir -p ./build/$GOOS/$GOARCH
+GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o ./build/$GOOS/$GOARCH
+
+GOOS=windows
+GOARCH=386
+
+mkdir -p ./build/$GOOS/$GOARCH
+GOOS=windows GOARCH=386 go  build -ldflags="-w -s" -o ./build/$GOOS/$GOARCH
